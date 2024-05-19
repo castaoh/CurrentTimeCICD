ce673b3c31e544f4c51655787ba770a52be77989
oscastan <oscastan@microsoft.com>
2024-05-19 10:19:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("time/utc", () => Results.Ok (DateTime.UtcNow));
+
+await app.RunAsync();
+
