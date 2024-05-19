feac803c2d0a50537c407691744c816a60edb240
oscastan <oscastan@microsoft.com>
2024-05-19 11:20:52 -0500

    Incluye segundo enpoint

--- Program.cs
+++ Program.cs
@@ -13,8 +13,33 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
 app.MapGet("time/utc", () => Results.Ok (DateTime.UtcNow));
 
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+
+
 await app.RunAsync();
 
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
